da7c07d626e3cc06eb4ec175482c67a395ae723a
Alexander Serditov <av.serditov@gmail.com>
2014-10-01 00:01:43 +0600

    add YACUKEN layout and change keyboard behavior on empty key

--- js/main.js
+++ js/main.js
@@ -7,18 +7,20 @@ require.config({
 require(["jquery", 'collections/layout', 'views/keyboard'], function($, layout, keyboard) {
     var kb = keyboard.initialize({
         '$container': $('.keyboard-container'),
-        layout: layout.LAYOUT_QWERTY
+        layout: layout.LAYOUT_YACUKEN
     }).render('text');
 
-    var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    var text = "Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона, а также реальное распределение букв и пробелов в абзацах, которое не получается при простой дубликации \"Здесь ваш текст.. Здесь ваш текст.. Здесь ваш текст..\" Многие программы электронной вёрстки и редакторы HTML используют Lorem Ipsum в качестве текста по умолчанию, так что поиск по ключевым словам \"lorem ipsum\" сразу показывает, как много веб-страниц всё ещё дожидаются своего настоящего рождения. За прошедшие годы текст Lorem Ipsum получил много версий. Некоторые версии появились по ошибке, некоторые - намеренно (например, юмористические варианты).";
     var $output = $('.console__output');
     highlightKey(text, 0);
 
     function highlightKey(text, index){
         var $dfd = kb.highlightKey('text', text[index]);
         $dfd.done(function(data){
-            $output.append(data.key['text']);
-            $output.scrollTop($output[0].scrollHeight - $output.height());
+            if(data.key) {
+                $output.append(data.key['text']);
+                $output.scrollTop($output[0].scrollHeight - $output.height());
+            }
             if(index < text.length-1) {
                 highlightKey(text, index+1);
             }
--- js/views/keyboard.js
+++ js/views/keyboard.js
@@ -5,11 +5,11 @@ define(function(require){
         SIZE_STANDARD: 'standard',
         SIZE_MEDIUM: 'medium',
         SIZE_LARGE: 'large',
+        ANIMATION_SPEED: 100,
         initialize: function(options){
             this.$container = options.$container;
-            this.ANIMATION_SPEED = 10;
             this.layout = options.layout;
-            options['animation_speed'] || (this.ANIMATION_SPEED = options['animation_speed']);
+            options['animation_speed'] && (this.ANIMATION_SPEED = options['animation_speed']);
 
             return this;
         },
@@ -36,17 +36,19 @@ define(function(require){
             var $dfd = $.Deferred();
 
             setTimeout($.proxy(function(){
-                var $keyboard = $('.keyboard');
-                if(this.activeKey) {
-                    $keyboard.find('.keyboard__row').eq(this.activeKey.posY)
-                        .find('div').eq(this.activeKey.posX)
-                        .removeClass('key_hightlighted');
+                if(key) {
+                    var $keyboard = $('.keyboard');
+                    if(this.activeKey) {
+                        $keyboard.find('.keyboard__row').eq(this.activeKey.posY)
+                            .find('div').eq(this.activeKey.posX)
+                            .removeClass('key_hightlighted');
+                    }
+                    $keyboard
+                        .find('.keyboard__row').eq(key.posY)
+                        .find('div').eq(key.posX)
+                        .addClass('key_hightlighted');
+                    this.activeKey = key;
                 }
-                $keyboard
-                    .find('.keyboard__row').eq(key.posY)
-                    .find('div').eq(key.posX)
-                    .addClass('key_hightlighted');
-                this.activeKey = key;
                 $dfd.resolve({'key': key});
             }, this), this.ANIMATION_SPEED);
 
